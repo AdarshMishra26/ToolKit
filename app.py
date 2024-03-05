c268424561fb3ad2dd3203d9e0e28c3897759d22
AdarshMishra26 <adarshmishra2608@gmail.com>
2024-03-06 02:36:13 +0530

    UI Improved

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('/index')
+def home():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/developer')
 def about():
-    return render_template('about.html')
+    return render_template('developer.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
